9ab92f775b97cf8c2dc127be2ed8aee53320a4e9
Kennethkilo <kennethwu.ubc@gmail.com>
2020-10-24 14:01:12 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          testing one two three
+        </p>
+        <a
+          className="App-link"
+          href="testestestsetset"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        
+          Learn React
+        </a>
+        <p>
+          asdfasdf
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
